bd85fa81c2dd10e201232c85e49a8226f843db81
Jonathan-Kane <jonathan.c.kane@gmail.com>
2022-06-29 12:42:37 -0500

    add css, img and js folders. navbar work

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        if (scroll >= 60) {
+            $(".navbar").addClass("bg-light");
+        } else {
+          $(".navbar").removeClass("bg-light");
+        }
+    });
+});
