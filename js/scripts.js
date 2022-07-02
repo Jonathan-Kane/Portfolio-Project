85cd17d77351183fb33383abc886142126d4e8ad
Jonathan-Kane <jonathan.c.kane@gmail.com>
2022-07-01 20:20:08 -0500

    clean up jQuery, remove About navbar link, finish out contact.html, edit p and form label text

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
+    $(window).on('scroll', function() {    
+        let scroll = $(window).scrollTop();
         if (scroll >= 70) {
             $(".navbar").css("background-color", "rgb(220, 220, 220)");
         } else {
